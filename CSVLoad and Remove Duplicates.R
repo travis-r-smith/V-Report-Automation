979d2ce2a705591edc9e2e952d9d89155b474191
travis-r-smith <48323417+travis-r-smith@users.noreply.github.com>
2020-01-03 07:58:11 -0600

    COV/COA 2 Field Update
    Partial success BUT it's erasing existing dates.

--- CSVLoad and Remove Duplicates.R
+++ CSVLoad and Remove Duplicates.R
@@ -3,8 +3,6 @@
   install.packages("tidyverse")
 #Add Library that enable removing duplicates
 library(tidyverse)
-#Add Library "dplyr" but I'm confused because it says dplyr is a part of tidyverse, whatevs.
-library(dplyr)
 library(magrittr)
 #Set the working Directory to RVeraAuto folder on G:Drive
 setwd("G:/My Drive/RVeraAuto")
@@ -24,9 +22,9 @@ Litig_df <- read.csv("LitigationTest.csv")
 main_df <- original_main_df
 main_df2 <- main_df
 #View(main_df)
+#str(main_df2)
 
-str(main_df2)
-#Remove duplicates from MainReport and store the value as 'MainUniq'
+#Remove duplicates from MainReport and store the value as 'main_df2'
 dim(main_df2)
 main_df2 <- main_df2[!duplicated(main_df$Matter.Case.ID), ]
 dim(main_df2)
@@ -38,9 +36,13 @@ activities_uniq_df <- activities_df[!duplicated(activities_df$Case.ID, activitie
 dim(activities_uniq_df)
 #View(ActUniq)
 
-#Store activity dates as actual dates 
+# STORE DATES AS DATES
+#Store activity and followup dates as actual dates 
 activities_uniq_df$Activity.Date <-as.Date(activities_uniq_df$Activity.Date, format = "%m/%d/%Y")
+class(activities_uniq_df$Activity.Date)
 
+main_df2$date_first_followup <-as.Date(main_df2$date_first_followup, format = "%m/%d/%Y")
+class(main_df2$date_first_followup)
 
 #Not necessary to remove duplicates from 1) HasAttny data set NOR 2) litigations
 
@@ -58,7 +60,7 @@ class(now)
 
 
 #USING REPORTING A# WHENEVER IT IS PRESENT AND A# WHEN THERE ISN'T A REPORTING A#
-#Remove hyphens from a number fields and store the column as integers
+# First Remove hyphens from a number fields and store the column as integers
 
 main_df2$Reporting.A.Number <- as.integer( gsub("-", "", main_df2$Reporting.A.Number))
 main_df2$a_number <- as.integer( gsub("-", "", main_df2$a_number))
@@ -85,7 +87,7 @@ HasAttny_df$new_a_number <- dplyr::coalesce(HasAttny_df$Reporting.A.Number, HasA
 activities_uniq_df$Item <- as.character( gsub("Sponsor Intake (Follow Up)", "Follow Up", activities_uniq_df$Item))
 activities_uniq_df$Item <- as.character( gsub("Released Referral- Letter to child", "Gave NGO Tel #s", activities_uniq_df$Item))
 
-#Re-sort data by Activity Item, Activity Date oldest on top, CaseID lowest on top, then A# lowest 
+#Re-Sort data by Activity Item, Activity Date oldest on top, CaseID lowest on top, then A# lowest 
 #on top as well.
 # Re-Sort dataframe by Activity Item (ascending), Activity Date (ascending), CaseID(ascending), and A# (ascending).
 # if you wanted to sort with descending just add a "-" (minus) in front of the data frame name no space
@@ -130,41 +132,210 @@ RefYoungCSub <- activities_uniq_df[activities_uniq_df$Item == "Young Center Refe
 
 #view(tail(NGOListSub))
 
+### PULL ACTIVITY DATES from Activities dataset into Main dataset based on Case ID#.
+
+#FOLLOW UPS
+#1st do a Precheck of what the values look like before importing
+main_df2[c(35:39, 7001), c(27, 104)]
+names(main_df2[c(27, 104)])
+
+#2nd pull all of the desired fields in
+main_df2 <- merge(main_df2, FollowUpSub[ , c("Item", "Activity.Date", "Case.ID")],
+                        by.x = "Matter.Case.ID.", by.y = "Case.ID", all.x = TRUE)
+
+#3rd Re-Format the activity and followup dates because the merge causes date to go back to factor
+main_df2$Activity.Date.x <-as.Date(main_df2$Activity.Date.x, format = "%m/%d/%Y")
+class(main_df2$Activity.Date.x)
+
+main_df2$date_first_followup <-as.Date(main_df2$date_first_followup, format = "%m/%d/%Y")
+class(main_df2$date_first_followup)
+
+#class(main_df2$date_first_followup) <- "Date"
+
+
+
+#Finally do the actual replacing process
+main_df2$date_first_followup <- ifelse(is.na(main_df2$date_first_followup), main_df2$Activity.Date.x, 
+                                             ifelse(main_df2$date_first_followup > main_df2$Activity.Date.x, 
+                                                    main_df2$Activity.Date.x, main_df2$date_first_followup )
+)
+#The above step messes with followup date format again so may have to re-class it. Question for Celia, does
+#the format of dates affect what date is considered less than or greatear than another?
+class(main_df2$date_first_followup) <- "Date"
+
+#Confirm successful replacements
+main_df2[c(35:39, 7001), c(27, 104)]
+names(main_df2[c(27, 104)])
+
+dim(main_df)
+dim(main_df2)
+names(main_df2[100:119])
+
+# REMOVE additional columns created during the subset merge for FollowUps
+main_df2 <- subset(main_df2, select = -c(Activity.Date.y, Activity.Date.x, Item))
+
+#---
+
+### PULL ACTIVITY DATES from Activities dataset into Main dataset based on Case ID#.
+
+#COV/COA's
+#1st do a Precheck of what the values look like before importing
+which( colnames(main_df2) == "cov_coa_date")
+
+main_df2[c(35:39, 7001), c(32, 33, 116)]
+names(main_df2[c(32, 33, 116)])
+
+#2nd pull all of the desired fields in
+main_df2 <- merge(main_df2, COVCOASub[ , c("Item", "Activity.Date", "Case.ID")],
+                  by.x = "Matter.Case.ID.", by.y = "Case.ID", all.x = TRUE)
+
+names(main_df2[100:118])
+#3rd Re-Format the activity and followup dates because the merge causes date to go back to factor
+main_df2$Activity.Date <-as.Date(main_df2$Activity.Date, format = "%m/%d/%Y")
+class(main_df2$Activity.Date)
+
+main_df2$date_first_followup <-as.Date(main_df2$date_first_followup, format = "%m/%d/%Y")
+class(main_df2$date_first_followup)
+
+#class(main_df2$date_first_followup) <- "Date"
+
+
+#THIS IS ALSO UNDER TESTING ******************************
+#Finally do the actual replacing process
+#It looks like NA values are white space, and there's a handful of "Facilitated Through Court Assistance" records. Not sure if either of those matter!
+
+#  This version handles the white space and other records by preserving the Detained UC value and setting the other original values to NA:
+##Tried it on 1.2.20 and it works with caveats. When I do it manually I get 742 COV/COA's total, including the 352 Formerly Detained.
+##When I do it with R I get 376 COV/COA's total. No Facilitated through court assistances and 352 formerly detained.
+##The Detained COV's still appear but the date get's wiped for most of them. Will rerun and investigate tomorrow.
+main_df2 <- main_df2 %>%
+  
+  mutate(cov_coa = as.character(cov_coa)) %>%
+  
+  mutate(
+    cov_coa_date = if_else(
+      is.na(date_first_followup),
+      Activity.Date,
+      
+      if_else(
+        date_first_followup > Activity.Date,
+        Activity.Date,
+        date_first_followup
+      )
+    ),
+    
+    cov_coa = if_else(
+      cov_coa == "Pro Se Assistance (Detained UC)",
+      cov_coa,
+      
+      if_else(
+        !is.na(cov_coa_date) &
+          cov_coa != "Pro Se Assistance (Detained UC)",
+        "Pro Se Assistance (Formerly Detained UC)",
+        as.character(NA)
+      )
+    )
+  )
+
+#Confirm successful replacements
+which( colnames(main_df2) == "cov_coa_date")
+which( colnames(main_df2) == "cov_coa")
+which( colnames(main_df2) == "Activity.Date")
+main_df2[c(30:45, 7001), c(32:33, 117, 118)]
+names(main_df2[c(31:33, 117:118)])
+
+
+
+
 #Testing Zone
 #---------------------------------------------------------------------------------------------------------
-#12.3.19 Attempt to pull Activity date from activities dataset into main dataset based on Case ID#.
+
+#This version preserves every value of cov_coa that doesn't fall within the if_else() parameter:
+
+main_df2 %>%
+  
+  mutate(cov_coa = as.character(cov_coa)) %>%
+  
+  mutate(
+    cov_coa_date = if_else(
+      is.na(date_first_followup),
+      Activity.Date,
+      
+      if_else(
+        date_first_followup > Activity.Date,
+        Activity.Date,
+        date_first_followup
+      )
+    ),
+    
+    cov_coa = if_else(
+      !is.na(cov_coa_date) &
+        cov_coa != "Pro Se Assistance (Detained UC)",
+      "Pro Se Assistance (Formerly Detained UC)",
+      cov_coa
+    )
+  )
+
+#It looks like NA values are white space, and there's a handful of "Facilitated Through Court Assistance" records. Not sure if either of those matter!
+
+#  This version handles the white space and other records by preserving the Detained UC value and setting the other original values to NA:
+
+main_df2 <- main_df2 %>%
+  
+  mutate(cov_coa = as.character(cov_coa)) %>%
+  
+  mutate(
+    cov_coa_date = if_else(
+      is.na(date_first_followup),
+      Activity.Date,
+      
+      if_else(
+        date_first_followup > Activity.Date,
+        Activity.Date,
+        date_first_followup
+      )
+    ),
+    
+    cov_coa = if_else(
+      cov_coa == "Pro Se Assistance (Detained UC)",
+      cov_coa,
+      
+      if_else(
+        !is.na(cov_coa_date) &
+          cov_coa != "Pro Se Assistance (Detained UC)",
+        "Pro Se Assistance (Formerly Detained UC)",
+        as.character(NA)
+      )
+    )
+  )
+
+
+
+
+#Reorganizing the if statement to change 2 columns?
+
+if (is.na(main_df2$cov_coa_date)) {
+  and(main_df2$cov_coa_date <-  main_df2$Activity.Date, main_df2$cov_coa <-  "Pro Se Assistance (Formerly Detained UC)")
+  }else{ 
+  ifelse(main_df2$cov_coa_date > main_df2$Activity.Date, 
+                                       main_df2$Activity.Date, main_df2$cov_coa_date)
+}
 #
-testVlookup_df <- merge(main_df2, FollowUpSub[ , c("new_a_number", "Item", "Activity.Date", "Case.ID")],
-                          by.x = "Matter.Case.ID.", by.y = "Case.ID", all.x = TRUE)
-view(head(testVlookup_df))
-
-testVlookup_df$date_first_followup <- as.Date(testVlookup_df$Activity.Date.x, format = "%m/%d/%Y")
-
+#
+#
+#I think next step is delete the Activity.Date.x column and any other additonal column then REPEAT for all 
+#data subsets.
+#
+#12.20 different might still try Celia's other suggestion if time allows
+testVlookup_df <- testVlookup_df %>%
+mutate(date_first_followup = casewhen(is.na(date_first_followup) ~ Activity.Date,
+                                      date_first_followup > Activity.Date ~ Activity.Date)
 
 #12.10.19 attempting to ONLY pull activity dates in when the column is NULL OR when the column has a value that has a later date.
-testVlookup_df$date_first_followup <- ifelse(is.null(testVlookup_df$date_first_followup) = TRUE,
+testVlookup_df$date_first_followup <- ifelse(is.null(testVlookup_df$date_first_followup) == TRUE,
                                              as.Date(testVlookup_df$Activity.Date.x, format = "%m/%d/%Y"),
-                                             ifelse(as.integer(testVlookup_df$date_first_followup) > as.integer(testVlookup_df$Activity.Date.x),
-                                                    as.Date(testVlookup_df$Activity.Date.x, format = "%m/%d/%Y"),)
-                                             )
-# testVlookup_df$date_first_followup <- if(is.null(testVlookup_df$date_first_followup) = TRUE),
-#                                              as.Date(testVlookup_df$Activity.Date.x, format = "%m/%d/%Y"),
-#                                              ifelse(as.integer(testVlookup_df$date_first_followup) > as.integer(testVlookup_df$Activity.Date.x),
-#                                                     as.Date(testVlookup_df$Activity.Date.x, format = "%m/%d/%Y"),
-#                                              ))
-
-?if
-#?is.null
-#?grepl
-#?ifelse
-#?'>'
-#stop()
-#Insert date values from Follow Ups if A# matches ???????????????
-#main_df2$date_first_followup <- format()
-#Not 100% sure 11.30.19
-#merge(main_df2, unique(activities_uniq_df)[, c("a_number", "Activity.Date")], by="a_number")
-#head(main_df2$Activity.Date)
-#names(main_df2)
+                                             ifelse(isTRUE(as.Date(testVlookup_df$date_first_followup, format = "%m/%d/%Y") > as.Date(testVlookup_df$Activity.Date.x, format = "%m/%d/%Y")) == TRUE,
+ 
 #
 #---------------------------------------------------------------------------------------------------------
 # FIN
